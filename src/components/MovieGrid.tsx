e50d3ebcdec4c61b672a75f895faf30b2c6ccbe2
Saied Sani <saied.sani.biderooni@gmail.com>
2023-06-16 01:59:59 +0200

    Create a Custom hook to fetch the movies

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,28 +1,8 @@
 import { Text } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-
-interface Movie {
-  id: number;
-  title: string;
-}
-
-interface FetchMovieResponse {
-  page: number;
-  results: Movie[];
-}
+import useMovies from "../hooks/useMovies";
 
 const MovieGrid = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchMovieResponse>("/popular")
-      .then((res) => setMovies(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  const { movies, error } = useMovies();
   return (
     <>
       {error && <Text>{error}</Text>}
