a91163c91b5b7a0e09014fdb9e2da6ccb2f7014e
Saied Sani <saied.sani.biderooni@gmail.com>
2023-06-17 00:13:35 +0200

    Displaying the Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,20 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import { BsChevronDown } from "react-icons/bs";
 
 const GenreList = () => {
   const { movieGenres } = useGenres();
   return (
-    <ul>
-      {movieGenres.map((movieGenre) => (
-        <li key={movieGenre.id}>{movieGenre.name}</li>
-      ))}
-    </ul>
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Movie Genres
+      </MenuButton>
+      <MenuList>
+        {movieGenres.map((movieGenre) => (
+          <MenuItem key={movieGenre.id}>{movieGenre.name}</MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
   );
 };
 
