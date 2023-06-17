0785db3386277e6d5e45b39a1faa06eb5445b824
Saied Sani <saied.sani.biderooni@gmail.com>
2023-06-17 16:24:59 +0200

    Build sort selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import MovieGrid from "./components/MovieGrid";
 import NavBar from "./components/NavBar";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import SortSelector from "./components/SortSelector";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -18,13 +19,15 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside">aside</GridItem>
+      </Show>
+      <GridItem area="main">
+        <HStack spacing={5} paddingLeft={2} marginY={5}>
           <GenreList
             onSelectGenre={(movieGenre) => setSelectedGenre(movieGenre)}
           />
-        </GridItem>
-      </Show>
-      <GridItem area="main">
+          <SortSelector />
+        </HStack>
         <MovieGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,21 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const SortSelector = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Sort by
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Item1</MenuItem>
+        <MenuItem>Item1</MenuItem>
+        <MenuItem>Item1</MenuItem>
+        <MenuItem>Item1</MenuItem>
+        <MenuItem>Item1</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
