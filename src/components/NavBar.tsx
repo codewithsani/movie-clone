c8a67e277b8bfba699a8c8d37db5fad22d6d52d3
Saied Sani <saied.sani.biderooni@gmail.com>
2023-06-19 17:26:45 +0200

    Refractoring Code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,21 +3,13 @@ import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
-import useMovieQueryStore from "../store";
 
 const NavBar = () => {
-  const searchText = useMovieQueryStore((s) => s.movieQuery.searchText);
   return (
     <HStack padding="20px">
-      {searchText ? (
-        <Link to="/" onClick={() => location.reload()}>
-          <Image src={logo} boxSize="70px" />
-        </Link>
-      ) : (
-        <Link to="/">
-          <Image src={logo} boxSize="70px" />
-        </Link>
-      )}
+      <Link to="/">
+        <Image src={logo} boxSize="70px" />
+      </Link>
 
       <SearchInput />
       <ColorModeSwitch />
